24b47a8102d126196f2fb5bb3d3569b8e624fae6
Eric Madureira <ericmadureira.uesc@gmail.com>
2020-04-20 15:34:16 -0300

    feat: page bar refactor
    Changes the page bar component name, writing and style.

--- src/components/PageBar.js
+++ src/components/PageBar.js
@@ -2,17 +2,20 @@ import React from 'react';
 
 import style from './PageBar.module.scss';
 
-const SearchBar = ({ hasSearchInput, label, setSearchTerm }) => (
-  <div className={style.searchBarContainer}>
+const PageBar = ({ hasSearchInput, label, setSearchTerm }) => (
+  <div className={style.pageBarContainer}>
     <span className={style.userLabel}>{label}</span>
-    <hr/>
-    {hasSearchInput && <input
-      placeholder='&#xF002; Filter by name or username'
-      type='text'
-      className='fas'
-      onChange={event => setSearchTerm(event.target.value.toLowerCase())}
-    />}
+    <hr className={style.separationLine} />
+    {
+      hasSearchInput &&
+      <input
+        placeholder='&#xF002; Filter by name or username'
+        type='text'
+        className={`fas ${style.searchBarInput}`}
+        onChange={event => setSearchTerm(event.target.value.toLowerCase())}
+      />
+    }
   </div>
 );
 
-export default SearchBar;
+export default PageBar;
