37557efc1dc24c52b7f9813a9f5b97befec146ac
Eric Madureira <ericmadureira.uesc@gmail.com>
2020-04-30 21:03:29 -0300

    fix: fix form buttons position
    Moves the submit and reset form buttons to the correct part of the
    layout.

--- src/components/AddUserForm.js
+++ src/components/AddUserForm.js
@@ -12,31 +12,33 @@ const AddUserForm = ({ addUser }) => {
       <hr/>
 
       <form className={style.addUserForm}>
-        <div className={style.formPart}>
-          <TextInput label='Username' instructions='Instructions' />
-          <TextInput label='Name' instructions='Instructions' />
-          <TextInput label='Email' instructions='Instructions' />
+        <div>
+          <div className={style.formPart}>
+            <TextInput label='Username' instructions='Instructions' />
+            <TextInput label='Name' instructions='Instructions' />
+            <TextInput label='Email' instructions='Instructions' />
+          </div>
 
-          <div>
-            <FormButton type='submit' label='Save' />
-            <FormButton type='reset' label='Discard' />
+          <div className={style.formPart}>
+            <TextInput label='City' instructions='Instructions' optional />
+            <MultiOptionInput
+              label={'Ride in group?'}
+              name={'groupRide'}
+              options={groupRideOptions}
+              type={'radio'}
+            />
+            <MultiOptionInput
+              label={'Days of the week'}
+              name={'weekDays'}
+              options={weekDays}
+              type={'checkbox'}
+            />
           </div>
         </div>
 
-        <div className={style.formPart}>
-          <TextInput label='City' instructions='Instructions' optional />
-          <MultiOptionInput
-            label={'Ride in group?'}
-            name={'groupRide'}
-            options={groupRideOptions}
-            type={'radio'}
-          />
-          <MultiOptionInput
-            label={'Days of the week'}
-            name={'weekDays'}
-            options={weekDays}
-            type={'checkbox'}
-          />
+        <div>
+          <FormButton type='submit' label='Save' />
+          <FormButton type='reset' label='Discard' />
         </div>
       </form>
     </div>
