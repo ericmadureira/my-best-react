02e12d1ff8159d814086c0b54bae4cb3b428c252
Eric Madureira <ericmadureira.uesc@gmail.com>
2020-06-02 00:00:30 -0300

    test: ListRow tests
    Adds tests for the ListRow component.

--- src/tests/components/ListRow.test.js
+++ src/tests/components/ListRow.test.js
@@ -15,3 +15,15 @@ test('renders provided username', () => {
   const username = getByText(/mockman/i);
   expect(username).toBeInTheDocument();
 });
+
+test('renders provided name', () => {
+  const { getByText } = render(
+    <table>
+      <tbody>
+        <ListRow user={mockUser} />
+      </tbody>
+    </table>
+  );
+  const name = getByText(/Mock User/i);
+  expect(name).toBeInTheDocument();
+});
