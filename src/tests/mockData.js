5488966546b0ab89cef16440c0b47f3c139c5bef
Eric Madureira <ericmadureira.uesc@gmail.com>
2020-06-01 23:54:09 -0300

    feat: mock data for tests
    Starts a new folder to store tests and adds mock data as helpers.

---
+++ src/tests/mockData.js
@@ -0,0 +1,16 @@
+const mockUser = {
+  albums: 10,
+  city: 'Campinas',
+  email: 'mock_user@test.com',
+  groupRideFrequency: 'Always',
+  id: 101,
+  name: 'Mock User',
+  photos: 20,
+  posts: 20,
+  username: 'mockman',
+  weekdaysRideFrequency: 'Every day',
+};
+
+module.exports = {
+  mockUser,
+};
